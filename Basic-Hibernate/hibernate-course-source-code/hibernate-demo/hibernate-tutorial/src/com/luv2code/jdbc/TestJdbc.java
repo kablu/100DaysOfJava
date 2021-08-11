c08bb00a37506a42823d4da647464f1660479234
satishppawar <pawar.satish2345@gmail.com>
2021-08-11 20:53:43 +0530

    2.Simple java code to print the all the table from DB

--- Basic-Hibernate/hibernate-course-source-code/hibernate-demo/hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ Basic-Hibernate/hibernate-course-source-code/hibernate-demo/hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -2,6 +2,11 @@ package com.luv2code.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Iterator;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class TestJdbc {
 
@@ -13,9 +18,15 @@ public class TestJdbc {
 		try {
 			System.out.println("Connecting to database: " + jdbcUrl);
 
-			//TODO Please update the password
+			// TODO Please update the password
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, null);
-
+			Statement statement= myConn.createStatement();
+			System.out.println("All databases available are:");
+			ResultSet resultset=statement.executeQuery("show databases");
+			while (resultset.next()) {
+				String dbName=resultset.getString(1);
+				System.out.println("Database Name is :"+dbName);
+			}
 			System.out.println("Connection successful!!!");
 
 		} catch (Exception exc) {
